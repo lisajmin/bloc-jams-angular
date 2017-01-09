f2c510b4d6daf864c103faf80eab8dfb5cbd7ef0
Lisa Min <lisajmin@gmail.com>
2017-01-08 21:51:01 -0800

    Checkpoint 3 - Routing and States: Complete

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,1 +1,23 @@
-angular.module('blocJams', []);
+(function() {
+    function config($stateProvider, $locationProvider) {
+        $locationProvider
+            .html5Mode({
+                enabled: true,
+                requireBase: false
+            });
+    
+        $stateProvider 
+            .state('landing', {
+                url: '/',
+                templateUrl: '/template/landing.html'
+            })
+            .state('album', {
+                url: '/album',
+                templateUrl: '/template/album.html'
+            });
+    }
+    angular
+        .module('blocJams', ['ui.router'])
+        .config(config);
+})();
+
